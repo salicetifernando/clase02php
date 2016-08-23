2655f8421a16dce8f38caa26ba7d2b19ab3751bb
salicetifernando <saliceti91@gmail.com>
2016-08-23 20:18:39 -0300

    asigno un valor a cada elemento del array.

--- aplicacionnro9.php
+++ aplicacionnro9.php
@@ -1,11 +1,19 @@
 <?php
 
 $lapicera  = array("color" => "" ,"marca" => "", "trazo" => "", "precio" => "" );
-/*
+
 foreach ($lapicera as $dato ) 
 {
-	$dato
+	
+	$dato["color"] = "rojo";
+	$dato["marca"] = "faber";
+	$dato["trazo"] = "grueso";
+	$dato["precio"] = "salado";
+	
+	//$dato["color"] = "rojo";
+	
 }
-*/
+
+
 
 ?>
